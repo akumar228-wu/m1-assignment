514ce9b64a8a9d749c4dc40c467ddda57a0a2a20
akumar228-wu <a.kumar.228@westcliff.edu>
2024-11-24 16:07:11 -0800

    using var in place of const

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const contactMethod = document.getElementById("contact-method");
-    const emailInput = document.getElementById("email-input");
-    const phoneInput = document.getElementById("phone-input");
+    var contactMethod = document.getElementById("contact-method");
+    var emailInput = document.getElementById("email-input");
+    var phoneInput = document.getElementById("phone-input");
 
     contactMethod.addEventListener("change", function () {
-        const selectedValue = contactMethod.value;
+        var selectedValue = contactMethod.value;
 
         emailInput.style.display = "none";
         phoneInput.style.display = "none";
